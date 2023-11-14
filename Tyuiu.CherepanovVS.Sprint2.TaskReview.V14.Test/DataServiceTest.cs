6f09604fc5ca333ae181c522e7d15281d3677a29
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-11-14 11:24:39 +0500

    Add project files.

---
+++ Tyuiu.CherepanovVS.Sprint2.TaskReview.V14.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.CherepanovVS.Sprint2.TaskReview.V14.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint2.TaskReview.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = -0.5;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.CherepanovVS.Sprint2.TaskReview.V14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint2.TaskReview.V14.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint2.TaskReview.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*Спринт2                                                                   *");
+            Console.WriteLine("*Тема: Review                                                              *");
+            Console.WriteLine("*Задание #Review                                                           *");
+            Console.WriteLine("*Вариант #14                                                               *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные            *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с координатами   *");
+            Console.WriteLine("* X,Y в заштрихованной области.                                            *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Введите значение переменной X =  ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            if (res)
+                Console.WriteLine("Точка находится в заштрихованной области");
+            else
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            Console.ReadKey();
+        }
+    }
+}
